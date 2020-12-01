612d9c6c6cf61c0876f88237ec0322c970fec68b
byrn8 <60743169+byrn8@users.noreply.github.com>
2020-12-01 14:49:06 +0000

    Add files via upload

---
+++ Pages/CurrencyConverter.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LB1.Pages
+{
+    public class CurrencyConverterModel : PageModel
+    {
+        public int amount;
+        public const double rate = 1.09;
+        public double result;
+
+        public void OnPost()
+        {
+            this.amount = Convert.ToInt32(Request.Form["amount"]);
+            this.result = amount * rate;
+        }
+    }
+}
---
+++ Pages/NameInput.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LB1.Pages
+{
+    public class NameInputModel : PageModel
+    {
+        public string name;
+
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPost()
+        {
+            this.name = Request.Form["name"];
+        }
+    }
+}
